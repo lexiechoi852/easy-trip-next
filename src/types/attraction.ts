16462a5ad20327ebefe2552b0adc0a9fd8125c4e
Lexie Choi <lexiechoi852@gmail.com>
2023-11-18 23:23:16 -0500

    feat: updated attraction and trip types

--- src/types/attraction.ts
+++ src/types/attraction.ts
@@ -2,12 +2,14 @@ export interface Attraction {
   id: number;
   name: string;
   description: string;
+  postalCode: string;
   address: string;
   placeId: string;
   latitude: number;
   longitude: number;
   image: string;
   url: string;
+  countryCode: string;
   telephone: string;
   city: string;
   openingHours: {
