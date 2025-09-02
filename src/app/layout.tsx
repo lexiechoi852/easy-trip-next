a610ba1a266d7c6ea418b06dacb37ede051a6890
Lexie Choi <lexiechoi852@gmail.com>
2025-09-02 17:28:29 -0400

    feat: added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import Header from "@/components/Header";
 import StoreProvider from "@/providers/store-provider";
 import GoogleAnalytics from "@/components/GoogleAnalytics";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -27,6 +28,7 @@ export default async function RootLayout({
           <Header />
           <main>{children}</main>
         </StoreProvider>
+        <Analytics />
       </body>
     </html>
   );
